b7db08232bfc02f4e3dcba0bd8671843cef5cc14
Paul Tobin <tobster111@gmail.com>
2020-08-10 19:38:56 +0100

    Change the default bio

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import { Styled } from 'theme-ui';
+
+const BioContent = () => (
+  <Fragment>
+    <p>Paul Tobin © 2020</p>
+  </Fragment>
+);
+
+export default BioContent;
